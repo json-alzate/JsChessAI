e80212f9712959fbd992c33a6a536ed8c14d33c0
json-alzate <developer.alzate@gmail.com>
2022-11-09 17:30:53 -0500

    🔖Random move

--- main.js
+++ main.js
@@ -3,80 +3,65 @@ let board = new ChessBoard('board', 'start');
 
 let game = new Chess();
 
+// pinta de las casillas para los posibles movimientos
 let highlightSquare =
     square => document.querySelector(`.square-${square}`).classList.toggle('highlight')
 
+// remueve el color de las casillas
 let removeHighlight =
     () => document.querySelectorAll('.highlight').forEach(el => el.classList.toggle('highlight'))
 
 
 // métodos
+
+// cuando se pasa el mouse por encima de una pieza se muestran los posibles movimientos
 let onMouseoverSquare =
     (square, piece) => {
         var moves = game.moves({ square, verbose: true }).map(({ to }) => to);
         [...moves, square].forEach(highlightSquare)
     }
 
+// cuando sale el mouse por encima de una casilla, se remueve el color de las casillas para los posibles movimientos
 let onMouseoutSquare = removeHighlight;
 
+// cuando se va a mover una pieza se valida si el juego no esta en jaque mate, 
+// en tablas o si el movimiento es es realizado por las blancas (vamos a jugar con blancas)
 let onDragStart =
     (square, piece) =>
         !(game.in_checkmate() || game.in_draw() || /^b/.test(piece))
 // /^b/.test(piece) is a black piece,
 
+
+// Cuando se suelta la pieza se valida si el movimiento es valido
 let onDrop = (from, to) => {
     removeHighlight();
     var move = game.move({ from, to, promotion: 'q' });
     if (!move) return 'snapback';
-    // renderHistory();
     window.setTimeout(makeBestMove, 250);
 
 }
 
 //----
 let makeBestMove = () => {
-    // findBestMove().then(bestMove => {
-    //     game.move(bestMove);
-    //     board.position(game.fen())
-    //     renderHistory();
-    // })
+    let bestMove = findBestMove();
+    if (bestMove) {
+        game.move(bestMove);
+        board.position(game.fen());
+    }
 }
 
 let findBestMove = () => {
     if (game.game_over()) return alert('Game over!');
-    return getBestMoveBasic()
+    return randomMove();
 }
 
-let getBestMoveBasic = () => {
-    let bestValue = -99999;
-    let bestMove = null;
-    for (let move of game.moves()) {
-        game.move(move);
-        let value = getBoardValue(game.board())
-        game.undo()
-        if (value > bestValue) {
-            bestValue = value
-            bestMove = move
-        }
-    }
-    return bestMove;
+// elige un movimiento aleatorio de los posibles movimientos
+let randomMove = () => {
+    let possibleMoves = game.moves();
+    let randomIndex = Math.floor(Math.random() * possibleMoves.length);
+    return possibleMoves[randomIndex];
 }
 
-let getBoardValue = (board) => {
-    return board.reduce(
-        (acc, row) => row.reduce((racc, piece) => piece ? racc + getPieceValue(piece) : racc, acc)
-        , 0);
-
-}
-
-let getPieceValue = ({ type, color }) => ({
-    k: 900,
-    q: 90,
-    r: 50,
-    b: 30,
-    n: 30,
-    p: 10
-}[type] * { w: -1, b: 1 }[color]);
 //---
 let onSnapEnd = () => {
     board.position(game.fen());
